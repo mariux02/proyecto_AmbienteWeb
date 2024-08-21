b40db2fd29ec742492ec996183c70ba66da44148
Raquel Vargas Meza <rvargas00519@ufide.ac.cr>
2024-08-21 11:16:02 -0600

    Estructura del proyecto y conexión a BD

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$servername = "localhost";
+$username ="root";
+$password = "";
+$database = "AUTOS";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if($conn->connect_error){
+    die("Conexion fallida: ".$conn->connect_error);
+}
